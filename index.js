5da3ae4374904b7dff637b884a1b21df55778977
ehpc <ehpc@ehpc.io>
2020-10-22 11:38:52 +0300

    Fix

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function crawlTree(tree) {
-  return tree.reduce((acc, x) => (acc + (Array.isArray(x) ? crawlTree(x) : x)), '');
-}
-
-function twoSum(arr, target) {
-  const map = {};
-  for (let i = 0; i < arr.length; i += 1) {
-    const val = arr[i];
-    if (typeof map[val] !== 'undefined') {
-      return [map[val], i];
-    }
-    const diff = target - val;
-    map[diff] = i;
-  }
-  return false;
-}
 
 module.exports = {
   crawlTree,
