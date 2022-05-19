efb0f978afdc2debf2da2ce33a880af7d8b4b0ef
Anton Valkovsky <ruizave@gmail.com>
2022-05-19 09:55:22 +0300

    частично решены

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 // Код для первого задания напиши в этой функции
 // Для правильного прохождения тестов рекурсивно вызывайте эту функцию так: functions.crawlTree
-function crawlTree() {}
+function crawlTree(el) {
+  if (Array.isArray(el)) {
+    let word = '';
+    for (let i = 0; i < el.length; i++) {
+      word += functions.crawlTree(el[i]);
+    }
+    return word;
+  }
+  return el;
+}
+
+tree = [
+  [
+    [['П', 'р'], 'и'],
+    ['в', ['е', 'т']],
+  ],
+  [['и', 'к'], 'и'],
+];
 
 // Код для второго задания напиши в этой функции
-function twoSum() {}
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i == j) {
+        continue;
+      }
+      if (arr[i] + arr[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+}
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
