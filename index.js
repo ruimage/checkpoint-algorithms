90aec6ccbf85e5cb49879e38582a22fc4de204f0
Anton Valkovsky <ruizave@gmail.com>
2022-05-19 10:16:37 +0300

    решение через  хешмап

--- index.js
+++ index.js
@@ -20,19 +20,36 @@ tree = [
 ];
 
 // Код для второго задания напиши в этой функции
+// function twoSum(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (i == j) {
+//         continue;
+//       }
+//       if (arr[i] + arr[j] == target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
 function twoSum(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (i == j) {
-        continue;
-      }
-      if (arr[i] + arr[j] == target) {
-        return [i, j];
-      }
+  const numbers = {};
+
+  arr.forEach((v, i) => {
+    if (numbers[v] == undefined) {
+      numbers[v] = i;
     }
+  });
+
+  for (let i = 0; i < arr.length; i++) {
+    const indSecond = numbers[target - arr[i]];
+    if (indSecond != undefined && indSecond != i) return [i, indSecond].sort((a, b) => a - b);
   }
 }
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
 module.exports = functions;
+
+twoSum([3, 2, 4], 6);
