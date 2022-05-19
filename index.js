cf332f6f79d9b509f7c46c992c7667bd35ce53ae
Anton Valkovsky <ruizave@gmail.com>
2022-05-19 10:19:09 +0300

    приведено в еслинт виду

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function crawlTree(el) {
   if (Array.isArray(el)) {
     let word = '';
+    // eslint-disable-next-line no-plusplus
     for (let i = 0; i < el.length; i++) {
       word += functions.crawlTree(el[i]);
     }
@@ -10,14 +11,6 @@ function crawlTree(el) {
   }
   return el;
 }
-
-tree = [
-  [
-    [['П', 'р'], 'и'],
-    ['в', ['е', 'т']],
-  ],
-  [['и', 'к'], 'и'],
-];
 
 // Код для второго задания напиши в этой функции
 // function twoSum(arr, target) {
@@ -37,19 +30,19 @@ function twoSum(arr, target) {
   const numbers = {};
 
   arr.forEach((v, i) => {
-    if (numbers[v] == undefined) {
+    if (numbers[v] === undefined) {
       numbers[v] = i;
     }
   });
 
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < arr.length; i++) {
     const indSecond = numbers[target - arr[i]];
-    if (indSecond != undefined && indSecond != i) return [i, indSecond].sort((a, b) => a - b);
+    if (indSecond !== undefined && indSecond !== i) return [i, indSecond].sort((a, b) => a - b);
   }
+  return [];
 }
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
 module.exports = functions;
-
-twoSum([3, 2, 4], 6);
